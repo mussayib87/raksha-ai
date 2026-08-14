78f9f44969ca1d4bb2a34128da9295540ed173d1
mussayib87 <mussayibibrahim@gmail.com>
2026-08-14 23:26:29 +0530

    Update App.jsx

--- scr/App.jsx
+++ scr/App.jsx
@@ -24,6 +24,7 @@ import {
   Zap,
 } from "lucide-react";
 import { useState } from "react";
+console.log("RAKSHA-AI APP LOADED");
 
 /* -------------------------------------------------------
    DEMO INCIDENT DATA
